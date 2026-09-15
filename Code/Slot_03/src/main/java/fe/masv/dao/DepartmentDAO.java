3803af566a1f1f625398da01a5c64f99ee00cb19
TranPhongQuyen <quyen@student.fpt.edu.vn>
2026-09-15 16:39:45 +0700

    TODO 2.5: Create JPAUtil and DAO classes for Department and Employee

---
+++ Code/Slot_03/src/main/java/fe/masv/dao/DepartmentDAO.java
@@ -0,0 +1,74 @@
+package fe.masv.dao;
+
+import fe.masv.pojo.Department;
+import fe.masv.util.JPAUtil;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+
+public class DepartmentDAO {
+
+    public void save(Department d) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(d);
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Department findById(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.find(Department.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Department> findAll() {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT d FROM Department d", Department.class)
+                    .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Department update(Department d) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Department updated = em.merge(d);
+            em.getTransaction().commit();
+            return updated;
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(Long id) {
+        EntityManager em = JPAUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            Department d = em.find(Department.class, id);
+            if (d != null) {
+                em.remove(d);
+            }
+            em.getTransaction().commit();
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+}
